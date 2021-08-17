87f8a5d0ddf0808744a52db0abf5a20b3efdad0d
arieuqis <siqueira.bcl@gmail.com>
2021-08-17 19:11:15 -0300

    Problem description added

--- src/test/java/org/dojolabs/Lab1Test.java
+++ src/test/java/org/dojolabs/Lab1Test.java
@@ -5,6 +5,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * Problem:
+ * This problem was recently asked by Google.
+ * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+ * For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+ * Bonus: Can you do this in one pass?
+
+ * @author Bruno
+ *
+ */
 public class Lab1Test {
 
 	@Test
